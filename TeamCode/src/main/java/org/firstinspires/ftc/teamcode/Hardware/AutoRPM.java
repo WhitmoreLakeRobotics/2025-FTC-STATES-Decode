f732eec9ee90e239a816d467c30805c58902fd2b
Titanium Trojans <whitmore.robot.8492@gmail.com>
2026-04-25 18:41:31 -0400

    Angle offset changes TJR

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/AutoRPM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/AutoRPM.java
@@ -55,10 +55,10 @@ public class AutoRPM {
 
         // Top motor interpolation
         double d1 = 0.6;       // meters
-        double r1top = 2210;
+        double r1top = 2160;
 
         double d2 = 2.24;       // meters
-        double r2top = 3250;
+        double r2top = 3225;
 
         double m_top = (r2top - r1top) / (d2 - d1);
         double b_top = r1top - m_top * d1;
@@ -66,8 +66,8 @@ public class AutoRPM {
         double targetTopRPM = m_top * Distance + b_top;
 
         // Bottom motor interpolation
-        double r1bottom = 2950;
-        double r2bottom = 3700;
+        double r1bottom = 2850;
+        double r2bottom = 3600;
 
         double m_bottom = (r2bottom - r1bottom) / (d2 - d1);
         double b_bottom = r1bottom - m_bottom * d1;
